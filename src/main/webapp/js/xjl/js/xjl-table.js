71c19d68e76878305da0625f785c82d7c7835e09
lilisheng <lilisheng@lilisheng-ThinkPad-X240>
2017-05-15 16:53:17 +0800

    重构js目录结构

--- src/main/webapp/js/xjl/js/xjl-table.js
+++ src/main/webapp/js/xjl/js/xjl-table.js
@@ -1,35 +1,4 @@
-var XJL = {
-		getUrlParam:function (name){
-			console.log("getUrlParam方法开始调用");
-			if (!this.urlParams){
-				console.log("urlParams为空，开始构建");
-				this.urlParams=new Object();   
-				var query=location.search.substring(1);//获取查询串   
-				var pairs=query.split("&");//在逗号处断开   
-				for(var   i=0;i<pairs.length;i++) {   
-						var pos=pairs[i].indexOf('=');//查找name=value   
-						if(pos==-1)   continue;//如果没有找到就跳过   
-						var argname=pairs[i].substring(0,pos);//提取name   
-						var value=pairs[i].substring(pos+1);//提取value   
-						this.urlParams[argname]=decodeURI(value);//存为属性
-						console.log("参数" +(i+1),argname +"="+ decodeURI(value));
-				}
-			}
-			console.log("返回",name + "的值" + this.urlParams[name]);
-			return this.urlParams[name];
-		},
-		bindCloseWindowEvent:function(){
-			console.log("绑定关闭窗口事件");
-			console.log("父窗口",window.opener);
-			console.log("parentEvent", this.getUrlParam("parentEvent"));
-			if (window.opener && this.getUrlParam("parentEvent")){
-				console.log("父窗口不为空，并且父窗口事件也不为空")
-				$(window).bind('beforeunload',function(){
-					var parentEvent = XJL.getUrlParam("parentEvent");
-					eval("window.opener."+parentEvent+"()");
-				});
-			}
-		},
+jQuery.extend(XJL,{
 		initTable:function (tableDiv,baseURL,pageSize) {
 			this.baseURL = baseURL;
 			pageSize = pageSize||10,
@@ -58,6 +27,14 @@ var XJL = {
 	        this.operateDelete(baseURL);
 	        
 	        this.domainModel =  jQuery.extend(true, {}, domainModel);
+	    },
+	    initOperate2:function (baseURL,fieldList) {
+	    	var fieldNameList = fieldList.split(",");
+	    	var domainModel = {};
+	    	$.each(fieldNameList,function(key,val){
+	    		domainModel[val]=ko.observable();
+	    	});
+	        this.initOperate(baseURL,domainModel);
 	    },
 	    //新增
 	    operateAdd: function(baseURL,domainModel){
@@ -151,4 +128,4 @@ var XJL = {
 	    queryParams:function(params){
 	    	return params;
 	    }
-}
+})
